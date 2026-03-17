75cee07a0d86f4efe692400db44d505c32d3e428
Akingbe Samuel <60920246+sakingbe@users.noreply.github.com>
2026-03-17 09:54:17 +0100

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -117,3 +117,25 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+$db['demo'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'mehjprhb_edumac',
+	'password' => '@1mW,N*9b0{cBBGM',
+	'database' => 'mehjprhb_edumac',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'developement'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
